f3ddbc0b0756cb5c811f53d85f701aea545119c6
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-01-30 21:00:51 -0800

    include aws template tests in CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ set -e -x -u
 ROOT_DIR_PATH=$(cd $(dirname $0)/.. && pwd)
 cd $ROOT_DIR_PATH
 
-ginkgo ./client ./commands ./controller ./shell
+ginkgo ./client ./commands ./controller ./shell ./aws/templates
